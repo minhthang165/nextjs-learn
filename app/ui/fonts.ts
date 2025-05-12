5adbf8e2073ed495cac403c36df3f3e7bac32b85
minhthang <minhthangqhqn@gmail.com>
2025-05-12 16:01:18 +0700

    up to chapter 10, dynamic rendering UI

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,20 @@
-import { Inter } from 'next/font/google';
+import { Be_Vietnam_Pro, Fira_Code, Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700'],
+});
+export const fontSans = Be_Vietnam_Pro({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+export const fontMono = Fira_Code({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: "swap",
 });
